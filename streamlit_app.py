14972dedb5209eb3b3543589d11848c2f6dc1a03
Shubhangi Shinde <shubhangishinde029@gmail.com>
2023-11-03 14:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,5 @@ streamlit.dataframe(my_fruit_list)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
